76fa7c57eade72e144422c1248046573b50a33cc
Keita Nakata <0024481294bxyk6037@gmail.com>
2022-09-18 06:31:22 +0900

    add: SE(Sound Effect)

--- Assets/Mato.cs
+++ Assets/Mato.cs
@@ -7,13 +7,14 @@ public class Mato : MonoBehaviour
 {
     [SerializeField] GameObject explosionPrefab;
     [SerializeField] private TextMeshProUGUI Text;
+    [SerializeField] AudioClip se;
 
     void OnCollisionEnter(Collision collision){
         Debug.Log(collision.gameObject.name); // ぶつかった相手の名前を取得
         if(collision.gameObject.name == "Bullet(Clone)"){
-            Debug.Log("hit");
-            Data.Instance.score++;
             Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+            AudioSource.PlayClipAtPoint(se, transform.position);
+            Data.Instance.score++;
             Text.text = "Hit: "+Data.Instance.score+"times";
         }else{
             Debug.Log("Hit other");
